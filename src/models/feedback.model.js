5124d5c5248e160ca560b9b15f0b17e6ec5b25be
Aishwary Dixit <mr.dixit17@gmail.com>
2025-04-07 18:28:54 +0530

    models created

---
+++ src/models/feedback.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const feedbackSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+        },
+
+        message: {
+            type: String,
+            required: true,
+        },
+
+    },
+    { timestamps : true }
+);
+
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+
+export default Feedback;
